0cc41ad63bfa3d10b711ffa5196784e62140b26d
meisler <mge987@mail.usask.ca>
2015-12-05 14:08:18 -0600

    Fixing

--- sender.c
+++ sender.c
@@ -29,7 +29,7 @@ void *get_in_addr(struct sockaddr *sa)
 int main(int argc, char *argv[])
 {
 	char *inputLine;
-	char *closeLine = "close";
+	char *closeLine = "close\r\n";
 	size_t buffer = 128;
 	/* int bytes_read; */
 
