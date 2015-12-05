8ce8760f7fcf05a169a7d8496b286580f2e131c0
meisler <mge987@mail.usask.ca>
2015-12-05 17:42:03 -0600

    Print out server IP?

--- server.c
+++ server.c
@@ -53,6 +53,24 @@ void *acceptSenders(){
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE; /* use my IP */
 
+	for(p = servinfo; p != NULL; p = p->ai_next){
+		void *addr;
+		char *ipver;
+
+		if(p->ai_family ==- AF_INET){
+			struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+			addr = &(ipv4->sin_addr);
+			ipver = "IPv4";
+		} else{
+			struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+			addr = &(ipv6->sin6_Addr);
+			ipver = "IPv6";
+		}
+
+		inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
+		printf(" %s: %s\n", ipver, ipstr);
+	}
+
 	gethostname(hostname, sizeof hostname);
 	printf("my hostname:  %s\n ", hostname);
 	getaddrinfo(hostname, INPORT, &hints, &servinfo);
