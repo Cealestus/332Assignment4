f43ee93117e47b6f5665d7a22b026825933b1521
meisler <mge987@mail.usask.ca>
2015-12-05 16:35:33 -0600

    Removed the loop.

--- server.c
+++ server.c
@@ -42,88 +42,85 @@ void sigchld_handler(int s) {
 
 void *acceptSenders(){
 	printf("Starting acceptSenders thread\n");
-	while(1){
-		struct addrinfo hints, *servinfo, *p;
-		struct sockaddr_storage their_addr; /* connector's address information */
-		socklen_t sin_size;
-		struct sigaction sa;
-		char s[INET6_ADDRSTRLEN];
-		int rv;
-		memset(&hints, 0, sizeof hints);
-		hints.ai_family = AF_UNSPEC;
-		hints.ai_socktype = SOCK_STREAM;
-		hints.ai_flags = AI_PASSIVE; /* use my IP */
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr; /* connector's address information */
+	socklen_t sin_size;
+	struct sigaction sa;
+	char s[INET6_ADDRSTRLEN];
+	int rv;
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; /* use my IP */
 
-		gethostname(hostname, sizeof hostname);
-		printf("my hostname:  %s\n ", hostname);
-		getaddrinfo(hostname, INPORT, &hints, &servinfo);
-		printf("Sender Port: %s\n ", INPORT);
-		printf("Receiver Port: %s\n ", OUTPORT);
-		if ((rv = getaddrinfo(NULL, INPORT, &hints, &servinfo)) != 0) {
-			fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+	gethostname(hostname, sizeof hostname);
+	printf("my hostname:  %s\n ", hostname);
+	getaddrinfo(hostname, INPORT, &hints, &servinfo);
+	printf("Sender Port: %s\n ", INPORT);
+	printf("Receiver Port: %s\n ", OUTPORT);
+	if ((rv = getaddrinfo(NULL, INPORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return;
+	}
+	/* loop through all the results and bind to the first we can */
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sendsockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol))
+				== -1) {
+			perror("server: socket");
+			continue;
+		}
+
+		if (setsockopt(sendsockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))
+				== -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+
+		if (bind(sendsockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sendsockfd);
+			perror("server: bind");
+			continue;
+		}
+
+		break;
+	}
+
+	freeaddrinfo(servinfo); /* all done with this structure */
+
+	if (p == NULL) {
+		fprintf(stderr, "server: failed to bind\n");
+		exit(1);
+	}
+
+	if (listen(sendsockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+	sa.sa_handler = sigchld_handler; /* reap all dead processes */
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		perror("sigaction");
+		exit(1);
+	}
+
+	while (1) { /*  main accept() loop */
+		sin_size = sizeof their_addr;
+		new_fd = accept(sendsockfd, (struct sockaddr *) &their_addr, &sin_size);
+		if (new_fd == -1) {
+			perror("accept");
+			continue;
+		}
+		inet_ntop(their_addr.ss_family,
+				get_in_addr((struct sockaddr *) &their_addr), s, sizeof s);
+		printf("server: got connection from %s\n", s);
+		if (!fork()) { /* this is the child process */
+			/* close(sockfd);  child doesn't need the listener*/
+			/*	if (send(wz2, "Hello, world!", 13, 0) == -1)
+			 perror("send");	*/
 			return;
 		}
-		/* loop through all the results and bind to the first we can */
-		for (p = servinfo; p != NULL; p = p->ai_next) {
-			if ((sendsockfd = socket(p->ai_family, p->ai_socktype,
-					p->ai_protocol)) == -1) {
-				perror("server: socket");
-				continue;
-			}
-
-			if (setsockopt(sendsockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-					sizeof(int)) == -1) {
-				perror("setsockopt");
-				exit(1);
-			}
-
-			if (bind(sendsockfd, p->ai_addr, p->ai_addrlen) == -1) {
-				close(sendsockfd);
-				perror("server: bind");
-				continue;
-			}
-
-			break;
-		}
-
-		freeaddrinfo(servinfo); /* all done with this structure */
-
-		if (p == NULL) {
-			fprintf(stderr, "server: failed to bind\n");
-			exit(1);
-		}
-
-		if (listen(sendsockfd, BACKLOG) == -1) {
-			perror("listen");
-			exit(1);
-		}
-		sa.sa_handler = sigchld_handler; /* reap all dead processes */
-		sigemptyset(&sa.sa_mask);
-		sa.sa_flags = SA_RESTART;
-		if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-			perror("sigaction");
-			exit(1);
-		}
-
-		while (1) { /*  main accept() loop */
-			sin_size = sizeof their_addr;
-			new_fd = accept(sendsockfd, (struct sockaddr *) &their_addr,
-					&sin_size);
-			if (new_fd == -1) {
-				perror("accept");
-				continue;
-			}
-			inet_ntop(their_addr.ss_family,
-					get_in_addr((struct sockaddr *) &their_addr), s, sizeof s);
-			printf("server: got connection from %s\n", s);
-			if (!fork()) { /* this is the child process */
-				/* close(sockfd);  child doesn't need the listener*/
-				/*	if (send(wz2, "Hello, world!", 13, 0) == -1)
-				 perror("send");	*/
-				return;
-			}
-			break;
-		}
+		break;
 	}
 }
 
